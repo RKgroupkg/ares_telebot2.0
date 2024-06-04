98b514302ddfe0524eab02a440ce67d559bf37a6
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2024-06-04 10:15:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import format_html
 import time,datetime
 import config
 
-import wikipedia,requests,base64
+import wikipedia,requests
 from wikipedia.exceptions import DisambiguationError, PageError
 
 from keep_alive import keep_alive
@@ -863,7 +863,7 @@ def create_image(prompt: str) -> bytes:
         >>> generated_image= ai_image("boy image")
         >>> print(generated_image)
         """
-        url =base64.b64decode('aHR0cHM6Ly9haS1hcGkubWFnaWNzdHVkaW8uY29tL2FwaS9haS1hcnQtZ2VuZXJhdG9y').decode("utf-8")
+        url = "https://ai-api.magicstudio.com/api/ai-art-generator"
 
         form_data = {
             'prompt': prompt,
